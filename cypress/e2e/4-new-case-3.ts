42c845089f13173d468547523c86581419a4a108
anthony <anthonyhoppy54@gmail.com>
2023-02-14 12:03:48 +0000

    commit 2

--- cypress/e2e/4-new-case-3.ts
+++ cypress/e2e/4-new-case-3.ts
@@ -1,1 +1,22 @@
 //yeni calisma
+
+/// <reference types="Cypress" />
+
+context('Actions', () => {
+    beforeEach(() => {
+      cy.visit('https://example.cypress.io/commands/actions')
+    })
+  
+    // https://on.cypress.io/interacting-with-elements
+  
+    it('.type() - type into a DOM element', () => {
+      // https://on.cypress.io/type
+      cy.get('.action-email')
+        .type('fake@email.com').should('have.value', 'fake@email.com')
+  
+        // .type() with special character sequences
+        .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
+        .type('{del}{selectall}{backspace}')
+  
+    })
+})
